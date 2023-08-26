c0532688b91bcec06f67704fa1b2b0ba0e53f7aa
Joao Marques <johnmarques.br2002@gmail.com>
2023-08-26 16:53:19 -0400

    ORGANIZED FOLDERS

--- PrizeManagement/src/app/app-routing.module.ts
+++ PrizeManagement/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ItemPageComponent } from './components/item-page/item-page.component';
-import { GroupsPageComponent } from './components/groups-page/groups-page.component';
+import { ItemPageComponent } from './components/views/item-page/item-page.component';
+import { GroupsPageComponent } from './components/views/groups-page/groups-page.component';
 
 const routes: Routes = [
   { path: "prizes", component: ItemPageComponent },
--- PrizeManagement/src/app/app.module.ts
+++ PrizeManagement/src/app/app.module.ts
@@ -4,20 +4,20 @@ import { FormsModule } from '@angular/forms'; // for template-driven forms
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ItemPageComponent } from './components/item-page/item-page.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 
 import { TreeTableModule } from 'primeng/treetable';
 import { ButtonModule } from 'primeng/button';
 import { TabMenuModule } from 'primeng/tabmenu';
-import { GroupsPageComponent } from './components/groups-page/groups-page.component';
 import { MenubarModule } from 'primeng/menubar';
 import { DialogModule } from 'primeng/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule } from 'primeng/calendar';
 import { PickListModule } from 'primeng/picklist';
 import { InputNumberModule } from 'primeng/inputnumber';
+import { ItemPageComponent } from './components/views/item-page/item-page.component';
+import { GroupsPageComponent } from './components/views/groups-page/groups-page.component';
 
 
 @NgModule({
